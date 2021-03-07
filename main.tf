941f29ce30d2a9d98a35470e99827b4e6dc1b0d0
ninotoshi <ninotoshi@users.noreply.github.com>
2021-03-07 18:43:50 +0900

    assume_role

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 provider "aws" {
   region = var.aws_region
+  assume_role {
+    role_arn    = var.aws_role_arn
+  }
 }
 
 provider "random" {}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-1"
+}
+
+variable "aws_role_arn" {
+  type    = string
 }
 
 variable "db_table_name" {
